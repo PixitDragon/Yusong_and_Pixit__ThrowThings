3ea6b719757ea633c9c7bf46958b19fe26b90748
rupesh0497 <user@FIH-COM23a102.fsu.edu>
2026-02-23 16:20:24 -0500

    started fall detection script

---
+++ Assets/Scripts/outOfBoundsDetection.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class outOfBoundsDetection : MonoBehaviour
+{
+    public float deathY = -2;
+
+    //[Tooltip("List of scripts to run on object death")]
+    //public int numberrrs;//runOnDeath[];
+
+    [Header("Item List")]
+    [Tooltip("List of items to detect if they fall out of bounds")]
+    public List<GameObject> itemsList = new List<GameObject>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if  fell out of bounds
+        //run scripts in list
+            //can include: tp to spawn, spawn new object, delete this object, etc. 
+
+        foreach(GameObject item in itemsList)
+        {
+            if (item != null)
+            {
+                if (transform.position.y < deathY)
+                {
+                    runDeathScripts();
+                }
+            }
+
+        }
+        
+        
+    }
+
+    void runDeathScripts()
+    {
+
+    }
+}
