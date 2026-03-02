35c6faf3fe78aea50f4b4696db9c2e6f54761338
rupesh0497 <user@FIH-COM23a102.fsu.edu>
2026-03-02 15:47:31 -0500

    Got random colors working, better!

--- Assets/Scripts/colorChangeToRGB.cs
+++ Assets/Scripts/colorChangeToRGB.cs
@@ -4,8 +4,8 @@ public class colorChangeToRGB : MonoBehaviour
 {
     public Material toChange;
     public Color newColor;
-    public void runColorChange(GameObject item, int red, int green, int blue) {
-        toChange.color = UnityEngine.Random.ColorHSV();
+    public void runColorChange(GameObject item, float red, float green, float blue) {
+        toChange.color = new Color(red/256,green/256,blue/256);
     }
 }
 
