10c82ed1994942a97272f1b0fa0cadefe43b72d2
rupesh0497 <user@FIH-COM23a102.fsu.edu>
2026-03-09 16:00:19 -0400

    Separated 3 goals and their color changing

--- Assets/Scripts/colliderHit.cs
+++ Assets/Scripts/colliderHit.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class colliderHit : MonoBehaviour
 {
+
+    public string diskNumber;
     public pointsManagement pointsAdd;
+    public colorChangeToRGB colChng;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Disk1")){
+        if (other.CompareTag(diskNumber)){
             Debug.Log("Disk entered goal!");
             //add point
             pointsAdd.addPoint();
             //reset disk
+            colChng.runColorChange(UnityEngine.Random.Range(0, 256), UnityEngine.Random.Range(0, 256), UnityEngine.Random.Range(0, 256)); ;
+
         }
     }
 
