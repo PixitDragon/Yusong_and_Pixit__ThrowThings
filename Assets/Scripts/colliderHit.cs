64da87a6dd0ced792f78a88886af7802357819eb
COM23a <user@FIH-COM23a101.fsu.edu>
2026-03-09 16:15:12 -0400

    goal now resets to random position after hit

--- Assets/Scripts/colliderHit.cs
+++ Assets/Scripts/colliderHit.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class colliderHit : MonoBehaviour
 {
     public pointsManagement pointsAdd;
+    Vector3 goalSpawnRegion;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Disk1")){
@@ -10,7 +11,14 @@ public class colliderHit : MonoBehaviour
             //add point
             pointsAdd.addPoint();
             //reset disk
+
+            //reset goal
+            resetGoalLocation();
         }
+    }
+    public void resetGoalLocation() {
+        goalSpawnRegion = new Vector3(Random.Range(4,-4), Random.Range(1, 2), Random.Range(5, 7));
+        this.transform.position= goalSpawnRegion;
     }
 
 }
